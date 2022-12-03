6d10447d178d3531a6abf165c969a3aaf79a26af
Вика Михайлова <C:\Users\hinat\AppData\Roaming\The Bat!>
2022-12-03 20:26:57 +0300

    Третья попытка

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-
 if (num1>num2)
     Console.WriteLine($"max = {num1} ");
 // if (num1<num2)
