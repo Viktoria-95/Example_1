ada4682b3d89a3a2269c3439b04a548a56f33681
Вика Михайлова <C:\Users\hinat\AppData\Roaming\The Bat!>
2022-12-03 21:12:28 +0300

    Пятая попытка

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
+
 if (num1>num2)
     Console.WriteLine($"max = {num1} ");
-    else Console.WriteLine($"max = {num2} ");
+    else if (num1<num2)
+    Console.WriteLine($"max = {num2} ");
+    else Console.WriteLine("Числа равны");
 
 // if (num1>num2)
 //     Console.WriteLine($"max = {num1} ");
